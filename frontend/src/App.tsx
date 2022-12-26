53efa4fd9d72d7fb43c8ffa2ed298923223c68f6
渡邉将太 <shotawebcoding@gmail.com>
2022-12-27 08:42:01 +0900

    push

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import {jsx} from "@emotion/react";
+import React from 'react'
+import SideBar from './SideBar';
+import * as Css from './sideBarStyle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	  <div style={{ display:'flex'}}>
+		  <div css={Css.sideBarContainer}>
+			  <SideBar />
+		  </div>
+		  <div css={Css.mainContainer}>aaa</div>
+		  
+	  </div>
+    
   );
 }
 
---
+++ frontend/src/sideBarStyle.tsx
@@ -0,0 +1,17 @@
+import { css } from "@emotion/react";
+
+export const sideBarContainer = css`
+
+width: 285px;
+min-width: 285px;
+height: 100vh;
+background-color: rgb(35, 48, 68);
+
+`;
+
+
+export const mainContainer = css`
+width: 100%;
+height: 100vh;
+
+`;
